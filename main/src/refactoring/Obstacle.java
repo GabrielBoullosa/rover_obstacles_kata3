bb9176f5419791707f0e32cbafb612e428fad9a3
GabrielBR16 <gabrieldiques@gmail.com>
2020-11-01 13:22:39 +0000

    I add the obstacle position test.

--- main/src/refactoring/Obstacle.java
+++ main/src/refactoring/Obstacle.java
@@ -4,21 +4,30 @@ public class Obstacle {
     private Dimensions dimensions;
     private Position position;
 
-    public Obstacle(int x, int y) {
-        dimensions = new Dimensions(x, y);
+    public Obstacle(Dimensions dimensions, int xPos, int yPos) {
+        this.dimensions = dimensions;
+        position = new Position(xPos, yPos);
     }
 
-    public int dimensionX() {
-        return dimensions.x_length();
+    public Obstacle(int xDim, int yDim, int xPos, int yPos) {
+        dimensions = new Dimensions(xDim, yDim);
+        position = new Position(xPos, yPos);
     }
+
+    public Obstacle(int xDim, int yDim, Position position) {
+        dimensions = new Dimensions(xDim, yDim);
+        this.position = position;
+    }
+
+    public int dimensionX() { return dimensions.x_length(); }
 
     public int dimensionY() {
         return dimensions.y_length();
     }
 
-    public String dimensions() {
-        return dimensions.toString();
-    }
+    public String dimensions() { return dimensions.toString(); }
+
+    public String position() { return position.toString(); }
 
     public static class Dimensions {
         private int x_length;
@@ -51,19 +60,6 @@ public class Obstacle {
         public Position(int x, int y) {
             this.x = x;
             this.y = y;
-        }
-
-        @Override
-        public boolean equals(Object object) {
-            return isSameClass(object) && equals((Obstacle.Position) object);
-        }
-
-        private boolean equals(Obstacle.Position position) {
-            return position == this || (x == position.x && y == position.y);
-        }
-
-        private boolean isSameClass(Object object) {
-            return object != null && object.getClass() == Rover.Position.class;
         }
 
     }
--- main/test/com/codemanship/marsrover/ObstacleRover_.java
+++ main/test/com/codemanship/marsrover/ObstacleRover_.java
@@ -9,10 +9,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ObstacleRover_ {
     @Test
     public void could_create_obstacles_with_an_dimension() {
-        Obstacle obstacle = new refactoring.Obstacle(3, 1);
+        Obstacle obstacle = new refactoring.Obstacle(3, 1, new Obstacle.Position(0,0));
         assertThat(obstacle.dimensionX()).isEqualTo(3);
         assertThat(obstacle.dimensionY()).isEqualTo(1);
         assertThat(obstacle.dimensions()).isEqualTo(new Obstacle.Dimensions(3, 1).toString());
-        assertThat(new Obstacle(5, 2).dimensions()).isEqualTo(new Obstacle.Dimensions(5, 2).toString());
+        assertThat(new Obstacle(5, 2, 3, 3).dimensions()).isEqualTo(new Obstacle.Dimensions(5, 2).toString());
+    }
+
+    @Test
+    public void could_create_obstacles_with_an_position() {
+        Obstacle obstacle = new refactoring.Obstacle(3, 1, new Obstacle.Position(0,0));
+        assertThat(obstacle.position()).isEqualTo(new Obstacle.Position(0, 0).toString()k);
+
     }
 }
