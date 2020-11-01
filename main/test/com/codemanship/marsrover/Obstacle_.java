01457f8cb42b6cdf60e82fd19136f3712b835b21
GabrielBR16 <gabrieldiques@gmail.com>
2020-11-01 14:05:57 +0000

    I rename the obstacle tests class.

--- main/test/com/codemanship/marsrover/Obstacle_.java
+++ main/test/com/codemanship/marsrover/Obstacle_.java
@@ -2,11 +2,10 @@ package com.codemanship.marsrover;
 
 import org.junit.Test;
 import refactoring.Obstacle;
-//import refactoring.Rover;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ObstacleRover_ {
+public class Obstacle_ {
     @Test
     public void could_create_obstacles_with_an_dimension() {
         Obstacle obstacle = new refactoring.Obstacle(3, 1, new Obstacle.Position(0,0));
