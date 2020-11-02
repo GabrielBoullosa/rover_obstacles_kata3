deb97d25380fb57827b2898bbc3c700569605f03
GabrielBR16 <gabrieldiques@gmail.com>
2020-11-02 14:46:27 +0000

    I add a new test to the rover not move if forward have an obstacle.

--- main/test/com/codemanship/marsrover/ObstacleRover_.java
+++ main/test/com/codemanship/marsrover/ObstacleRover_.java
@@ -6,6 +6,7 @@ import refactoring.Rover;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static refactoring.Rover.Heading.North;
+import static refactoring.Rover.Order.Forward;
 
 public class ObstacleRover_ {
 
@@ -18,6 +19,7 @@ public class ObstacleRover_ {
         assertThat(new Rover("North", 5, 5).heading()).isEqualTo(North);
         assertThat(new Rover("North", 5, 5).position()).isEqualTo(new Rover.Position(5,5));
     }
+
     @Test
     public void could_add_obstacles_to_rover_obstacle_list() {
         Obstacle obstacle1 = new Obstacle(2, 2, 1, 1);
@@ -29,6 +31,17 @@ public class ObstacleRover_ {
         assertThat(rover.Obstacle(1)).isEqualTo(obstacle2);
     }
 
-
+    @Test
+    public void could_not_the_rover_move_if_forward_have_an_obstacle() {
+        Obstacle obstacle1 = new Obstacle(1, 1, 1, 2); // (1, 2) -> (1, 2)
+        Obstacle obstacle2 = new Obstacle(2, 3, -2, -3); // (-1, -2) -> (0, 0)
+        Rover rover = new Rover("N", 1, 1);
+        rover.obstacleAdd(obstacle1);
+        rover.obstacleAdd(obstacle2);
+        rover.go(Forward);
+        assertThat(rover.position()).isEqualTo(new Rover.Position(1, 1));
+        rover.go("BLF");
+        assertThat(rover.position()).isEqualTo(new Rover.Position(0, 1));
+    }
 
 }
